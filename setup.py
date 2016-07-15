dcb25e783de88f9b90a5d48868e801118c84a059
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2016-07-15 11:28:01 +0100

    Issue #2: release candidate 3 now

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='16.07rc2',
+    version='16.07rc3',
 
     description='A compact GUI application for optical distortion calibration of endoscopes',
     long_description=long_description,
