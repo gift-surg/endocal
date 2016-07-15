7689646b262ee4e469031dfa514c469e2fb40ba1
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2016-07-15 11:58:30 +0100

    Issue #2: now installing package data as a subfolder of package, release candidate 8

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='16.07rc7',
+    version='16.07rc8',
 
     description='A compact GUI application for optical distortion calibration of endoscopes',
     long_description=long_description,
@@ -83,7 +83,7 @@ setup(
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    package_data={'endocal': ['../data/sample_001/*']},
+    package_data={'endocal': ['data/sample_001/*']},
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
