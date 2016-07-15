8173487b68f111e8a387c1c792716d01301a0404
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2016-07-15 11:55:15 +0100

    Issue #2: this installs data at the same level as package, release candidate 7

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='16.07rc5',
+    version='16.07rc7',
 
     description='A compact GUI application for optical distortion calibration of endoscopes',
     long_description=long_description,
@@ -66,7 +66,10 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    packages=['endocal'],
+
+    # As recommended in https://docs.python.org/2/distutils/setupscript.html#installing-package-data
+    package_dir={'endocal': 'endocal'},
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
@@ -80,7 +83,7 @@ setup(
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    package_data={'endocal': ['data/sample_001/*']},
+    package_data={'endocal': ['../data/sample_001/*']},
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
