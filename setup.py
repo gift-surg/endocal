fa3bdbccecc2be02ab017c7605093793b2f49ecd
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2016-07-15 09:49:15 +0100

    Issue #1: added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -65,6 +65,15 @@ setup(
     # simple. Or you can use find_packages().
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
 
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['PyYAML', 'numpy'],
+
+    # External dependencies not on PyPI
+    dependency_links=['http://opencv.org/'],
+
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
