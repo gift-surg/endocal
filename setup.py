cba47fc45f18acb455809f4901995adc17ba631c
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-02-01 09:11:17 +0000

    Issue #26: changed version to 18.02.01.dev1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='16.07.28rc8',
+    version='18.02.01.dev1',
 
     description=summary,
     long_description=long_description,
