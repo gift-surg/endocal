5f8d04fc009ef80c198d33edb9c8f0eddd8628fa
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-02-13 10:33:09 +0000

    Changed version string to 18.02.13.dev0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='18.02.01.dev1',
+    version='18.02.13.dev0',
 
     description=summary,
     long_description=long_description,
