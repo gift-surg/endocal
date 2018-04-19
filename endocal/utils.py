8e4bbde16c4012d08bb5b7d29fa34c44a6cf3a23
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-04-19 10:11:18 +0100

    Issue #8: added utility function for extracting numbers from YML-encoded numpy array

--- endocal/utils.py
+++ endocal/utils.py
@@ -6,3 +6,12 @@ def check_positive_int(value):
     if ivalue <= 0:
         raise ArgumentTypeError('{} is not a positive value'.format(value))
     return ivalue
+
+
+def extract_numbers(num_str):
+    """Extract all available numerical values from passed string.
+
+    :rtype: list
+    """
+    _num_str = num_str.strip('[ ]').replace('[', '').replace(']', '').replace('\n', '').replace('\\', '')
+    return list(map(float, _num_str.split()))
